9a6b3eca39faf684115344edb7e34ce23c3f181d
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-08-31 19:11:16 -0500

    documente y corregi el error de tener saldo negativo

--- Tarjeta.java
+++ Tarjeta.java
@@ -2,11 +2,18 @@
 public class Tarjeta
 {
     private float saldo;
+    /**
+     * En este metodo inicializamos la tarjeta con un credito de 0
+     */
     public Tarjeta()
     {
         saldo = 0;
     }
     
+    /**
+     *En este metodo ingresamos una cantidad a la cuenta de la tarjeta
+     *
+     */
     public void Depositar(float dinero)
     {
         if (saldo > 0){
@@ -15,17 +22,28 @@ public class Tarjeta
         
     }
     
+    /**
+     * Este metodo retira dinero de la tarjeta, revisa que no se saque mas de lo que tiene
+     */
     public float retirar(float dinero)
     {
-        if(saldo>=dinero)
-          {
-              saldo = saldo - dinero;
-              return(dinero);
-          }
-        else 
-        return 0; 
+        if(saldo > 0)
+       {
+            if(saldo>=dinero)
+              {
+                saldo = saldo - dinero;
+                return(dinero);
+              }
+            else 
+                return 0; 
+        }
+        else
+           return 0;
     }
     
+    /**
+     * Este metodo solo muestra el saldo que tiene
+     */
     public void consulta()
     {
         System.out.println(saldo);
